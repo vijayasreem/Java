5c500d65fb1685b8f220288c3cf1634b1087f227
root <root@1ebc78b89098>
2024-02-17 12:19:26 +0000

    pushed_from_larcas

--- src/main/java/com/Technical/userstories/code/service/MortgageLoanService.java
+++ src/main/java/com/Technical/userstories/code/service/MortgageLoanService.java
@@ -1,11 +1,11 @@
-The API should be able to retrieve the car mortgage loan application data from the database.
-
 package com.Technical.userstories.code.service;
 
 import com.Technical.userstories.code.model.MortgageLoan;
 import com.Technical.userstories.code.repository.MortgageLoanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class MortgageLoanService {
@@ -15,18 +15,26 @@ public class MortgageLoanService {
 
     // To receive and store car mortgage loan application data from customers.
     public MortgageLoan saveMortgageLoanData(MortgageLoan mortgageLoan) {
-        return mortgageLoanRepository.save(mortgageLoan);
+        if (validateMortgageLoanData(mortgageLoan)) {
+            MortgageLoan savedMortgageLoan = mortgageLoanRepository.save(mortgageLoan);
+            sendConfirmationMessage(savedMortgageLoan);
+            return savedMortgageLoan;
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid mortgage loan data");
+        }
     }
 
     // To validate the accuracy and completeness of the application data.
     public boolean validateMortgageLoanData(MortgageLoan mortgageLoan) {
         // Business logic to validate mortgage loan data
+        // Add your validation logic here
         return true;
     }
 
     // To send a confirmation message to the customer upon successful submission of the application.
     public void sendConfirmationMessage(MortgageLoan mortgageLoan) {
         // Send confirmation message logic
+        // Add your logic to send confirmation message to the customer
     }
 
     // To retrieve the car mortgage loan application data from the database.
