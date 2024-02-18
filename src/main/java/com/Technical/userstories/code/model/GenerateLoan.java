664274762f65f0b83ec44a3c9b90807d25165120
root <root@73e5829870ac>
2024-02-18 14:52:02 +0000

    pushed_from_larcas

--- src/main/java/com/Technical/userstories/code/model/GenerateLoan.java
+++ src/main/java/com/Technical/userstories/code/model/GenerateLoan.java
@@ -12,21 +12,27 @@ public class GenerateLoan {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String customerId;
+    private String customerName;
     private String applicationForm;
     private String loanOffer;
     private String acceptedLoanOffer;
     private String approvedLoanAmount;
+    private String customerAccountNumber;
+    private String bankIFSCCode;
 
     public GenerateLoan() {
     }
 
-    public GenerateLoan(Long id,String customerId, String applicationForm, String loanOffer, String acceptedLoanOffer, String approvedLoanAmount) {
+    public GenerateLoan(Long id, String customerId, String customerName, String applicationForm, String loanOffer, String acceptedLoanOffer, String approvedLoanAmount, String customerAccountNumber, String bankIFSCCode) {
         this.id = id;
         this.customerId = customerId;
+        this.customerName = customerName;
         this.applicationForm = applicationForm;
         this.loanOffer = loanOffer;
         this.acceptedLoanOffer = acceptedLoanOffer;
         this.approvedLoanAmount = approvedLoanAmount;
+        this.customerAccountNumber = customerAccountNumber;
+        this.bankIFSCCode = bankIFSCCode;
     }
 
     public Long getId() {
@@ -43,6 +49,14 @@ public class GenerateLoan {
 
     public void setCustomerId(String customerId) {
         this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
     }
 
     public String getApplicationForm() {
@@ -75,5 +89,21 @@ public class GenerateLoan {
 
     public void setApprovedLoanAmount(String approvedLoanAmount) {
         this.approvedLoanAmount = approvedLoanAmount;
+    }
+
+    public String getCustomerAccountNumber() {
+        return customerAccountNumber;
+    }
+
+    public void setCustomerAccountNumber(String customerAccountNumber) {
+        this.customerAccountNumber = customerAccountNumber;
+    }
+
+    public String getBankIFSCCode() {
+        return bankIFSCCode;
+    }
+
+    public void setBankIFSCCode(String bankIFSCCode) {
+        this.bankIFSCCode = bankIFSCCode;
     }
 }
